ecf26ada4f4d995e6fbe55c3235edbf0c96f15c2
hunter297 <edidimubarak@gmail.com>
2024-12-28 18:27:14 +0100

    classwork on brief on javascript

---
+++ script.js
@@ -0,0 +1,33 @@
+var D = 10.9999
+var I = "boxes"
+var D = null
+var I = true
+
+
+document.write(typeof (D))
+document.write("<br>")
+document.write(typeof (I))
+document.write("<br>")
+document.write(typeof (D))
+document.write("<br>")
+document.write(typeof (I))
+document.write("<br>")
+
+
+var k = true
+
+var cars = ["bugatti", "venza", "lamboghini"]
+var age = 25
+if (age >= 30) {
+    document.write("You are old enough")
+}
+
+else {
+    document.write("you are young")
+}
+document.write("<br>")
+let i = 1
+while (i <= 10) {
+    document.write(i)
+    i++;
+}
